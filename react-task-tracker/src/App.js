c31a701724d54a5363e4dbda5e6614ef75a4c84f
Tony <tonyngmengkiat@gmail.com>
2022-12-30 20:39:07 +0800

    first commit

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  const name = 'Tony';
+  return (
+    <div className="app">
+      <h1>Hello from React</h1>
+      <h2>Hello {name}</h2>
+    </div>
+  );
+}
+
+export default App;
